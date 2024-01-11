153d920fa7fa48e217cdf94f914d55501c75cedc
Milena Lara <milenalrf@gmail.com>
2024-01-10 22:07:36 -0300

    generics

--- src/main/java/br/com/ada/pooii/aula02/generics/Caixa.java
+++ src/main/java/br/com/ada/pooii/aula02/generics/Caixa.java
@@ -1,13 +1,22 @@
 package br.com.ada.pooii.aula02.generics;
 
-public class Caixa<TIPO> {
-  private TIPO obj;
+public class Caixa<T> {
+  /*
+  * E - Element (used extensively by the Java Collections Framework)
+  * K - Key
+  * N - Number.
+  * T - Type
+  * V - Value
+  * S,U, V etc. - 2nd, 3rd, 4th types
+  */
 
-  public Caixa(TIPO obj) {
+  private T obj;
+
+  public Caixa(T obj) {
     this.obj = obj;
   }
 
-  public TIPO getObj(){
+  public T getObj(){
     return obj;
   }
 
